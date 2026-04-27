35d70b15f79d0c0b30b73762ba51c9b3c2da0f57
Matheus Castilho - Desenvolvimento Back-end Contratare - BNE <matheuscastilho@bne.com.br>
2026-04-26 21:48:42 -0300

    fix(settings): fixing default ports

--- src/GoodHamburger.AppHost/Program.cs
+++ src/GoodHamburger.AppHost/Program.cs
@@ -1,8 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var api = builder.AddProject<Projects.GoodHamburger_Api>("api");
+var api = builder.AddProject<Projects.GoodHamburger_Api>("api")
+                 .WithEndpoint("http", e => e.Port = 5001);
 
 builder.AddProject<Projects.GoodHamburger_Web>("web")
+       .WithEndpoint("http", e => e.Port = 5002)
        .WithReference(api)
        .WithEnvironment("ApiBaseUrl", api.GetEndpoint("http"));
 
